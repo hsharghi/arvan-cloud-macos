33ca57d2e92a1f34516c1a5a6c96bb195c7b6ab7
Hadi Sharghi <hsharghi@gmail.com>
2019-12-26 23:25:37 +0330

    Initial Commit

---
+++ ArvanCloud/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArvanCloud
+//
+//  Created by Hadi Sharghi on 12/26/19.
+//  Copyright © 2019 Hadi Sharghi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArvanCloud/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArvanCloud
+//
+//  Created by Hadi Sharghi on 12/26/19.
+//  Copyright © 2019 Hadi Sharghi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
